61ef7a318fdea44f488a5ce8dd4233e2b039c8bc
commondevelopernp <56087940+commondevelopernp@users.noreply.github.com>
2025-02-12 19:31:52 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/TpprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpprojectApplication.class, args);
+	}
+
+}
